330a1f1129addf9fe277688b52948f9ac5b02b0e
Student <Student@ADMIN-PC>
2023-05-21 11:31:35 +0300

    Add project files.

---
+++ RegularExam/Perfume.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegularExam
+{
+    class Perfume
+    {
+        private string brand;
+        private double price;
+        private List<Perfume> perfumes;
+        public string Brand
+        {
+            get { return this.brand; }
+            set { this.brand = value; }
+        }
+        public double Price
+        {
+            get { return this.price; }
+            set {
+                if (value > 100)
+                {
+                    throw new ArgumentException($"Invalid perfume price!");
+                }
+                this.price = value; }
+        }
+        public Perfume (string brand, double price)
+        {
+            this.Brand = brand;
+            this.Price = price;
+        }
+        public void AddPerfume(Perfume perfume) {
+            this.perfumes.Add(perfume);
+        }
+        public override string ToString()
+        {
+            StringBuilder printPerfumeInfo = new StringBuilder();
+            foreach (var perfume in perfumes)
+            {
+                printPerfumeInfo.Append($"Perfume {this.Brand} costs {this.Price}");
+            }
+            return printPerfumeInfo.ToString();
+        }
+    }
+}
---
+++ RegularExam/Perfumery.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegularExam
+{
+    class Perfumery
+    {
+        private string name;
+        private List<Perfume> perfumes;
+        public string Name
+        {
+            get { return this.name; }
+            set {
+                if (value.Length < 6)
+                {
+                    throw new ArgumentException($"Invalid perfumery name!");
+                }
+                this.name = value; }
+        }
+        public Perfumery(string name)
+        {
+            this.Name = name;
+            this.perfumes = new List<Perfume>();
+        }
+        public override string ToString()
+        {
+            StringBuilder printPerfumeryInfo = new StringBuilder();
+
+            if (this.perfumes.Count == 0)
+            {
+                return $"Perfumery {this.Name} has no available perfumes.";
+            }
+
+            printPerfumeryInfo.Append($"Perfumery {this.Name} has {this.perfumes.Count} perfume/s:");
+
+            foreach (var perfume in perfumes)
+            {
+                printPerfumeryInfo.Append($"\nPerfume {perfume.Brand} costs {perfume.Price.ToString("0.00")}");
+            }
+            return printPerfumeryInfo.ToString();
+        }
+        public void AddPerfume(Perfume perfume)
+        {
+            this.perfumes.Add(perfume);
+        }
+        public bool SellPerfume(Perfume perfume)
+        {
+            Perfume perfume1 = perfumes.Where(x => x.Brand == perfume.Brand).First();
+            return this.perfumes.Remove(perfume1);
+        }
+        public double CalculateTotalPrice()
+        {
+            return this.perfumes.Sum(x=>x.Price);
+        }
+        public void RenamePerfumery(string newName)
+        {
+            this.Name = newName;
+        }
+        public void SellAllPerfumes()
+        {
+            this.perfumes = new List<Perfume>();
+        }
+        public Perfume GetPerfumeWithHighestPrice()
+        {
+            return this.perfumes.OrderByDescending(x => x.Price).First();
+        }
+        public Perfume GetPerfumeWithLowestPrice()
+        {
+            return this.perfumes.OrderBy(x => x.Price).First();
+        }
+    }
+}
